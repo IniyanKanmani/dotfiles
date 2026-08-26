dfbf355a835a20d27386632af3a17f51ca72c89a
IniyanKanmani <kiniyan002@gmail.com>
2026-08-26 17:16:57 +0530

    switched yazi to flavors and updated plugins

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,5 +1,3 @@
-require("git"):setup()
-
 th.git = th.git or {}
 th.git.modified_sign = "M"
 th.git.added_sign = "A"
@@ -8,6 +6,16 @@ th.git.ignored_sign = "!"
 th.git.deleted_sign = "D"
 th.git.updated_sign = "M"
 
-require("full-border"):setup()
+require("git"):setup()
+
+require("full-border"):setup({ type = ui.Border.ROUNDED })
 
 require("relative-motions"):setup({ show_numbers = "relative", show_motion = true })
+
+require("duckdb"):setup({
+	mode = "standard", -- Default: "summarized"
+	cache_size = 1000, -- Default: 500
+	row_id = true, -- Default: false
+	minmax_column_width = 21, -- Default: 21
+	column_fit_factor = 10.0, -- Default: 10.0
+})
